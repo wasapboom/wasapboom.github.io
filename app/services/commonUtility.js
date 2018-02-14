5135cd2dfcad47a1918ac82f9139bb212b35880f
Mohammed Mansoor <pingtomansoor@gmail.com>
2018-02-14 14:11:03 +0800

    start

--- app/services/commonUtility.js
+++
@@ -1,8 +0,0 @@
-app.factory('CommonUtility', function ($window) {
-    var root = {};
-
-    //    root.server_url = "https://mysite-saml.maybank.com.my/";
-    root.server_url = "https://websvc.mysite.maybank.com.my/";
-
-    return root;
-});
